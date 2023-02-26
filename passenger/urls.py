6a4c962607d07b5bc9913616cf90cf114562ebc0
Johnson Gabriel <johnsongabimoisili2003@gmail.com>
2023-02-27 00:54:10 +0100

    Configured the new templates and static files

--- passenger/urls.py
+++ passenger/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homePage, name="home")
+    path('', views.homePage, name="home"),
+    path('gallery', views.galleryPage, name="gallery")
 ]
--- passenger/views.py
+++ passenger/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def homePage(request):
-    return render(request, 'passenger/home.html')
+    return render(request, 'passenger/index.html')
+
+def galleryPage(request):
+    return render(request, 'passenger/gallery.html')
