feb07b15b53c59c34e41071829d74e33d789d2ca
Ezekiel--dev <81721664+Easy-creator@users.noreply.github.com>
2023-03-02 10:08:55 +0100

    Update settings.py

--- dts_luxury_transport/settings.py
+++ dts_luxury_transport/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'dts_luxury_transport.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'passenger/templates/passenger'],
+        'DIRS': [BASE_DIR / 'passenger/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'passenger/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR/, "passenger/static/passenger")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "passenger/static/")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
