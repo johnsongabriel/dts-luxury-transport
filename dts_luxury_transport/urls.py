b35e83907b782d2e52d09acd45c24d29e62eace4
Ezekiel--dev <81721664+Easy-creator@users.noreply.github.com>
2023-03-05 17:18:21 +0100

    edited

--- dts_luxury_transport/urls.py
+++ dts_luxury_transport/urls.py
@@ -1,8 +1,17 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('passenger.urls'))
+    path('', include('passenger.urls')),
+
+    path('verify/', include('main.urls', namespace="main")),
+    path('users/', include('users.urls', namespace="users")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
