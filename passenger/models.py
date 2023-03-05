b81d6b1f92b74328152657d23332c604cfef7085
Ezekiel--dev <81721664+Easy-creator@users.noreply.github.com>
2023-03-05 17:30:05 +0100

    update on this

--- passenger/models.py
+++ passenger/models.py
@@ -3,13 +3,20 @@ from datetime import datetime
 # Create your models here.
 
 class BookingDB(models.Model):
-    service_detail = models.CharField(max_length=50)
+    service_detail = models.CharField(max_length=50, blank=False)
     pick_up_date = models.DateField(auto_now=False, blank=False)
     pick_up_time = models.TimeField(auto_now=False, blank=False)
-    number_of_passenger = models.IntegerField()
-    number_of_luggages = models.IntegerField()
-    pick_up_address = models.CharField(max_length=100)
-    drop_off_address = models.CharField(max_length=100)
+    number_of_passenger = models.IntegerField(blank=False)
+    number_of_luggages = models.IntegerField(blank=False)
+    pick_up_address = models.CharField(max_length=150, blank=False)
+    drop_off_address = models.CharField(max_length=150, blank=False)
+
+    way_point_1 = models.CharField(max_length=250, blank=True)
+    way_point_2 = models.CharField(max_length=250, blank=True)
+
+    #eazy-creator
+    airline = models.CharField(max_length=200)
+    flight_number = models.IntegerField()
 
     def __str__(self):
         return f"{self.service_detail}"
