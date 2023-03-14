46f26a941bb482898983272ff7dc478b172df414
Ezekiel--dev <81721664+Easy-creator@users.noreply.github.com>
2023-03-14 23:44:31 +0100

    Add files via upload

--- passenger/models.py
+++ passenger/models.py
@@ -1,57 +1,22 @@
 from django.db import models
-from django.utils import timezone
-
+from datetime import datetime
 # Create your models here.
 
 class BookingDB(models.Model):
-    #id = models.AutoField(primary_key=True)
-    service_details = models.CharField(max_length=50, blank=False)
+    service_detail = models.CharField(max_length=50, blank=False)
     pick_up_date = models.DateField(auto_now=False, blank=False)
-    pick_up_time_hour = models.CharField(max_length=100, blank=False)
-    pick_up_time_mins = models.CharField(max_length=100, blank=False)
+    pick_up_time = models.CharField(max_length=50,blank=False)
     number_of_passenger = models.IntegerField(blank=False)
     number_of_luggages = models.IntegerField(blank=False)
-    pick_up_address = models.CharField(max_length=150, null=True, blank=True)
-    dropoff_address = models.CharField(max_length=150, null=True, blank=True)
-    dropoff_airport = models.CharField(max_length=150, null=True, blank=True)
-    pickup_airport = models.CharField(max_length=150, null=True, blank=True)
-    
-
+    pick_up_address = models.CharField(max_length=150, blank=False)
+    drop_off_address = models.CharField(max_length=150, blank=False)
 
     way_point_1 = models.CharField(max_length=250, blank=True)
     way_point_2 = models.CharField(max_length=250, blank=True)
 
     #eazy-creator
     airline = models.CharField(max_length=200)
-    work_hour = models.CharField(max_length=100, blank=True, null=True)
-    flight_number = models.IntegerField(blank=True)
-
-    # Personal Information
-    first_name = models.CharField(max_length=200, blank=False)
-    last_name = models.CharField(max_length=200, blank=False)
-    phone_number = models.IntegerField()
-    email = models.EmailField(blank=False)
-    
-    def __str__(self):
-        return f"{self.service_detail} | {self.pick_up_date} | {self.first_name}:{self.last_name}"
-    
-
-
-#newsletter and subcriber
-class Subcriber(models.Model):
-    email = models.EmailField(unique=True, max_length=100)
-    date_created = models.DateTimeField(default=timezone.now)
-
-    def __str__(self) -> str:
-        return self.email
-
-
-#Contact/Feedback Model
-class ContactFeedback(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.EmailField(blank=False)
-    message = models.TextField(max_length=400)
-    date_created = models.DateTimeField(default=timezone.now, null=True)
+    flight_number = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
-        return f"Name: {self.name} | Contact On: {self.date_created}"
+        return f"{self.service_detail}"
--- passenger/views.py
+++ passenger/views.py
@@ -1,120 +1,76 @@
 from django.shortcuts import render, redirect
+from datetime import datetime, timedelta
 from django.contrib import messages
 from django.conf import settings
-from .models import BookingDB, Subcriber, ContactFeedback #, PersonalInfoBooking
-
-from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
+from .models import BookingDB
 # Create your views here.
 
 
 
 def homePage(request):
-    if request.method == "POST":
-        service_details = request.POST['service_details']
+    if request.method == 'POST':
+        service_detail = request.POST['service_details']
         pick_up_date = request.POST['pick_up_date']
-        pick_up_time_hour = request.POST['pick_up_time_hour']
-        pick_up_time_mins = request.POST['pick_up_time_mins']
+        pick_up_time = request.POST['pick_up_time']
         number_of_passenger = request.POST['number_of_passenger']
         number_of_luggages = request.POST['number_of_luggages']
         pick_up_address = request.POST['google_address_a']
-        dropoff_address = request.POST['google_address_b']
-        pickup_airport = request.POST['pickup_airport']
-        dropoff_airport = request.POST['dropoff_airport']
+        drop_off_address = request.POST['google_address_b']
 
 
-        # way_point_1 = request.POST['google_address_c']
-        # way_point_2 = request.POST['google_address_d']
-
+        way_point_1 = request.POST['google_address_c']
+        way_point_2 = request.POST['google_address_d']
 
         airline = request.POST["airline"]
-        work_hour = request.POST["work_hour"]
         flight_number = request.POST["flight_number"]
 
-        if BookingDB.objects.filter(pick_up_date=pick_up_date).exists() and BookingDB.objects.filter(pick_up_time_hour=pick_up_time_hour).exists() and BookingDB.objects.filter(pick_up_time_mins=pick_up_time_mins).exists():
+        if BookingDB.objects.filter(pick_up_date=pick_up_date).exists() and BookingDB.objects.filter(pick_up_time=pick_up_time).exists():
             messages.info(request, 'This Time has already been booked')
             return redirect('home')
 
         else:
             bookingForm = BookingDB.objects.get_or_create(
-                service_details = service_details, 
+                service_detail = service_detail, 
                 pick_up_date = pick_up_date,
-                pick_up_time_hour = pick_up_time_hour, 
-                pick_up_time_mins = pick_up_time_mins,
+                pick_up_time = pick_up_time, 
                 number_of_passenger = number_of_passenger, 
                 number_of_luggages = number_of_luggages,
                 pick_up_address = pick_up_address, 
-                dropoff_address = dropoff_address,
-                pickup_airport = pickup_airport,
-                dropoff_airport = dropoff_airport,
+                drop_off_address = drop_off_address,
                 airline = airline,
-                work_hour = work_hour,
                 flight_number = flight_number,
 
 
-                # way_point_1 = way_point_1,
-                # way_point_2 = way_point_2,
+                way_point_1 = way_point_1,
+                way_point_2 = way_point_2,
             )
             messages.info(request, "let's proceed")
             print('Booking saved')
             return redirect('home')
-        
     context = {
 	    "google_api_key": settings.GOOGLE_API_KEY,
         "base_country": settings.BASE_COUNTRY
         }
+
     return render(request, 'passenger/index.html', context)
 
 
 
+def galleryPage(request):
+    return render(request, 'passenger/gallery.html')
+
 def car_selection(request):
     return render(request, 'passenger/select.html')
 
+def complete_booking(request):
+    return render(request, 'passenger/user_details.html')
+
+def terms_condition(request):
+    return render(request, 'term_and_conditions.html')
 
 
-#subcriber logic for newsletter
-def home(request):
-    if request.method == "POST":
-        #get the email from the input field
-        email = request.POST['email']
+def privacy(request):
+    return render(request, 'privacy.html')
 
-        #if already a subcriber dont notify and dont safe
-        if Subcriber.objects.filter(email=email).first():
-            messages.error(request, f'{email} is already a subcriber')
-            return redirect('home_subcribe')
-        
-        try:
-            validate_email(email)
-        except ValidationError as e:
-            messages.error(request, e.messages[0])
-            print('Error')
-            return redirect("home_subcribe")
-        
-        #if pass all condition save to the database
-        subcriber = Subcriber.objects.get_or_create(email=email)
-        messages.success(request, f'{email} successfully subscribed to our newsletter!')
-        print('succesfull')
-        return redirect('home_subcribe')
-    return render(request, 'home.html')
-
-
-#Logic for contact form
-def contact(request):
-    if request.method == "POST":
-        name = request.POST['name']
-        email = request.POST['email']
-        message = request.POST['message']
-
-        contact = ContactFeedback.objects.get_or_create(
-            name=name,
-            email=email,
-            message=message
-        )
-        messages.success(request, f"Thanks for contacting us")
-        print('thanks for contacting us')
-        return redirect('contact')
+def contact_us(request):
     return render(request, 'passenger/contact.html')
-
-
-def galleryPage(request):
-    return render(request, 'passenger/gallery.html')
