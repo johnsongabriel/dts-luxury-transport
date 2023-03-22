233f3d993c8114850fb4990f4cf1bbceb9637b1c
Johnson Gabriel <johnsongabimoisili2003@gmail.com>
2023-03-22 21:24:45 +0100

    password reset done | operational error fixed

--- dts_luxury_transport/urls.py
+++ dts_luxury_transport/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 from django.conf.urls.static import static
 
@@ -13,6 +14,10 @@ urlpatterns = [
     #path('users/', include('users.urls', namespace="users")),
     path('users/', include('user_base.urls', namespace="users")),
     path('rentals/', include('rentals.urls', namespace="rentals")),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="user/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), name='password_reset_complete'),      
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
--- user_base/urls.py
+++ user_base/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 from django.contrib.auth import views as auth_views	
 from django.views.generic import TemplateView
-
-from .forms import UserLoginForm,PwdResetForm,PwdResetConfirmForm
+from django.contrib.auth import views as auth_views
+from .forms import UserLoginForm #,PwdResetForm,PwdResetConfirmForm
 
 
 app_name = "users"
@@ -21,30 +21,31 @@ urlpatterns = [
 	path('profile/delete/', views.delete_user, name="delete_user"),
     path('profile/delete_confirm/', TemplateView.as_view(template_name="user/delete_confirm.html"), name='delete_confirmation'),
 	path('activate/<slug:uidb64>/<slug:token>', views.account_activate, name="activate"),
+    path("password_reset", views.password_reset_request, name="password_reset"),
 
 
-	#path('password_reset/', auth_views.PasswordResetView.as_view(template_name="user/password_reset_form.html",success_url='password_reset_email_confirm',email_template_name='user/password_reset_email.html',form_class=PwdResetForm), name='pwdreset'),
-#
-	#path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html',success_url='/users/password_reset_complete/', form_class=PwdResetConfirmForm),name="password_reset_confirm"),
-    #path('password_reset/password_reset_email_confirm/',TemplateView.as_view(template_name="user/reset_status.html"), name='password_reset_done'),
-    #path('password_reset_complete/',TemplateView.as_view(template_name="user/reset_status.html"), name='password_reset_complete'),
+	# path('password_reset/', auth_views.PasswordResetView.as_view(template_name="user/password_reset_form.html",success_url='password_reset_email_confirm',email_template_name='user/password_reset_email.html',form_class=PwdResetForm), name='pwdreset'),
+
+	# path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html',success_url='/users/password_reset_complete/', form_class=PwdResetConfirmForm),name="password_reset_confirm"),
+    # path('password_reset/password_reset_email_confirm/',TemplateView.as_view(template_name="user/reset_status.html"), name='password_reset_done'),
+    # path('password_reset_complete/',TemplateView.as_view(template_name="user/reset_status.html"), name='password_reset_complete'),
 
 
 
 
-    # Reset password
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="user/password_reset_form.html",
-                                                                 success_url='password_reset_email_confirm',
-                                                                 email_template_name='user/password_reset_email.html',
-                                                                 form_class=PwdResetForm), name='pwdreset'),
-    path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html',
-                                                                                                success_url="/users/password_reset_complete/", 
-                                                                                                form_class=PwdResetConfirmForm),
-         name="password_reset_confirm"),
-    path('password_reset/password_reset_email_confirm/',
-         TemplateView.as_view(template_name="user/reset_status.html"), name='password_reset_done'),
-    path('password_reset_complete/',
-         TemplateView.as_view(template_name="user/reset_status.html"), name='password_reset_complete'),
+    # # Reset password
+    # path('password_reset/', auth_views.PasswordResetView.as_view(template_name="user/password_reset_form.html",
+    #                                                              success_url='password_reset_email_confirm',
+    #                                                              email_template_name='user/password_reset_email.html',
+    #                                                              form_class=PwdResetForm), name='pwdreset'),
+    # path('password_reset_confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html',
+    #                                                                                             success_url="/users/password_reset_complete/", 
+    #                                                                                             form_class=PwdResetConfirmForm),
+    #      name="password_reset_confirm"),
+    # path('password_reset/password_reset_email_confirm/',
+    #      TemplateView.as_view(template_name="user/reset_status.html"), name='password_reset_done'),
+    # path('password_reset_complete/',
+    #      TemplateView.as_view(template_name="user/reset_status.html"), name='password_reset_complete'),
 
 
-	]
+]
