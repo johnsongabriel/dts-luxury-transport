dcd051ca543757379b83963ad1daed18e55bfa72
Ezekiel--dev <81721664+Easy-creator@users.noreply.github.com>
2023-03-25 20:47:16 +0100

    Add files via upload

--- rentals/models.py
+++ rentals/models.py
@@ -78,67 +78,6 @@ class RentForm(models.Model):
 			total = int(self.price) * cal_c
 		return total
 
-	def calculate_total_ones(self):
-		if self.Days and not self.hourss:
-			con_v = int(self.Days) * 24
-			total = self.price * con_v
-		elif self.hourss != '' and self.Days == '':
-			total = int(self.hourss) * self.price
-
-			return total
-
-	def calculate_total(self):
-		if isinstance(self.price, str):
-			self.price = int(self.price)
-		if not isinstance(self.Days, str):
-			self.Days = ''
-			self.Days = str(self.Days)
-		if not isinstance(self.hourss, str):
-			self.hourss = str(self.hourss)
-			self.hourss = ''
-		if isinstance(self.Days, str):
-			if self.Days == '':
-				self.Days = '1'
-			else:
-				self.Days = int(self.Days)
-			self.Days = int(self.Days) * 24
-			total = self.Days * self.price
-		if isinstance(self.hourss, str):
-			if self.hourss == '':
-				self.hourss = '1'
-			else:
-				self.hourss = int(self.hourss)
-			total = self.hourss * self.price
-		return total
-
-	def calculate_total_one(self):
-		if isinstance(self.price, str):
-			self.price = int(self.price)
-		if isinstance(self.Days, str):
-			self.Days = int(self.Days) if self.Days else 0
-		if isinstance(self.hourss, str):
-			self.hourss = int(self.hourss) if self.hourss else 0
-		if self.Days and not self.hourss:
-			total = self.Days * self.price
-		elif self.hourss and not self.Days:
-			total = self.hourss * self.price
-		else:
-			total = 0
-		return total
-
-	def get_total_price(self):
-		tota = self.price
-		total = int(tota)
-		return total
-
-	def get_hours(self):
-		hours = self.hourss
-		return hours
-
-	def dayes(self):
-		daay = self.Days
-		return daay
-
 	class Meta:
 		verbose_name = "Rents Form"
 		ordering = ['-date']
